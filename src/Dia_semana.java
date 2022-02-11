601243045b66572ec2e72340a71d6d72dd759b34
Bernabe <bernabeite@gmail.com>
2022-02-11 17:24:24 -0500

    realización del octavo programa

---
+++ src/Dia_semana.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Dia_semana {
+    public static void main(String[] args) {
+        Scanner Captura = new Scanner(System.in);
+        System.out.println("Ingrese el dia a consultar");
+        String Dia = Captura.next();
+        Dia= Dia.toLowerCase();
+
+        switch (Dia){
+            case "lunes" :
+            case "martes":
+            case "miercoles":
+            case "jueves":
+            case "viernes":
+                System.out.println("el dia : "+Dia+ " es un dia laboral");
+                break;
+            case "sabado":
+            case "domingo":
+                System.out.println("el dia :"+Dia+" es un dia no laborable");
+                break;
+        }
+    }
+}
