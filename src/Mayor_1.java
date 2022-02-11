885fcc423d4fa65ea69767467daf604990e62679
Bernabe <bernabeite@gmail.com>
2022-02-11 17:08:55 -0500

    realización del primer programa

---
+++ src/Mayor_1.java
@@ -0,0 +1,14 @@
+public class Mayor_1 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        if( x<y){
+            System.out.println(+x+ " es menor que " + y);
+        }
+        else if(x==y){
+            System.out.println(" los valores son iguales.");
+        }
+        else System.out.println( +x +" es mayor que " +y);
+    }
+}
+
